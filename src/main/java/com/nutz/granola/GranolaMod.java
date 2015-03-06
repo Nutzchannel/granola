17c95ed6ac4d2255556d61f28392c02248a59c2f
nutzchannel <spencie01@gmail.com>
2015-03-06 16:37:20 -0500

    Basic @Mod, gradle crap
    Added mod class, @Nutzchannel package (keep), Reference.java
    STABLE

---
+++ src/main/java/com/nutz/granola/GranolaMod.java
@@ -0,0 +1,29 @@
+package com.nutz.granola;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class GranolaMod {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
